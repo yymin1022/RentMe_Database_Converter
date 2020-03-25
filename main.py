e10ef50ad45188e73517af077f3b4716ecb6844b
yymin1022 <yymin1022@gmail.com>
2020-03-25 18:07:32 +0900

    Update JSON Parse Code

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import firebase_admin
+import json
 from firebase_admin import credentials
 from firebase_admin import firestore
-from bs4 import beautifulsoup4
+from urllib.request import urlopen
 
 project_id = "rent-me-1d154"
 
@@ -13,12 +14,19 @@ firebase_admin.initialize_app(cred, {
 
 db = firestore.client()
 
-parseUrl1 = "http://api.data.go.kr/openapi/rntcarentrps-std?serviceKey=9eag1d1n%2FBOSl2FTYMUGz6J9uxN3JtSP%2BJtmKOEl%2FY%2FdcHdX%2Fg02MLYG1PMvZa1k8S5lQ7P%2Bz4BOlIaxyZOLYA%3D%3D&pageNo="
+parseUrl1 = "http://api.data.go.kr/openapi/rntcarentrps-std?serviceKey=APIKEY&pageNo="
 parseUrl2 = "&numOfRows=100&type=json"
 
-doc_ref = db.collection(u'companies').document(u'alovelace')
-doc_ref.set({
-    u'first': u'Ada',
-    u'last': u'Lovelace',
-    u'born': 1815
-})
+for i in range(22):
+    response = urlopen(parseUrl1 + str(i + 1) + parseUrl2).read().decode('utf-8')
+    strJson = json.loads(response)
+
+    for j in range(100):
+        print(strJson.get("response").get("body").get("items")[j])
+
+# doc_ref = db.collection(u'companies').document(u'alovelace')
+# doc_ref.set({
+#     u'first': u'Ada',
+#     u'last': u'Lovelace',
+#     u'born': 1815
+# })
